6c390b5c2af2895ee71bc2972c538f45ecff0911
minhhuyhahaha <39334276+minhhuyhahaha@users.noreply.github.com>
2018-09-06 09:59:30 +0700

    Update database.js

--- my_module/database.js
+++ my_module/database.js
@@ -1,14 +1,19 @@
 var mysql = require('mysql');
 var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "mysql",
-    database: "batdongsan"
+    host: "mysql:3306",
+    user: "crawler",
+    password: "12345",
+    database: "crawlerdb"
 });
   
 con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
+    let sql = "CREATE TABLE IF NOT EXISTS `advertising` (`id` int(11) NOT NULL PRIMARY KEY,`url` text,`title` text,`content` text,`image` text,`info_estate` text,`info_project` text,`name` text,`address` text,`email` text,`phone` text,`area` text,`price` text,`area_size` text) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+    con.query(sql, (err, results, fields) => {
+        if (err) throw err;
+        console.log(results);
+    });
 });
 
 function insert(data){
